3fed66a61c8f4f279156f940d170acc68c388d46
Thomas Lopez <lopeztomaa0@gmail.com>
2022-11-05 13:30:53 -0300

    Agregando funcionalidad al carrito

--- js/tienda.js
+++ js/tienda.js
@@ -6,8 +6,6 @@ const botonBuscar = document.getElementById("botonBuscar")
 let productos = []
 let carrito = []
 
-botonCarrito.addEventListener("click", () => console.log(carrito))
-botonBuscar.addEventListener("click", () => buscarProducto(inputBuscador.value))
 localStorage.setItem("carrito", JSON.stringify(productos));
 
 const comprarProducto = (producto) => {
@@ -24,28 +22,19 @@ const comprarProducto = (producto) => {
             cantidad: 1
         })
     }
-
-    console.log(carrito)
 }
 
 const buscarProducto = (string) => {
 
-    console.log(string);
-
     let productoBuscado = productos.find(producto => producto.nombre === string);
-
-    console.log(productoBuscado);
-
-    console.log(productoBuscado.nombre);
     inputBuscador.value = ''
 }
 
-/* Eliminar producto */
-const eliminar = (nombre) => {
+const eliminarProducto = (nombre) => {
     let carrito = JSON.parse(localStorage.getItem("carrito"));
     carrito = carrito.filter(item => item.nombre != nombre);
     localStorage.setItem("carrito", JSON.stringify(carrito));
-};
+}
 
 const iniciarTienda = async () => {
     const resp = await fetch('../data/data.json')
@@ -75,13 +64,91 @@ const iniciarTienda = async () => {
                 text: "Has agregado "+producto.nombre+" al carrito!", 
                 icon: "success",
                 confirmButtonText: "Aceptar",
-            });
-
+            })
             comprarProducto(producto)
         });
-
-        
     })
 }
 
 iniciarTienda()
+
+botonCarrito.addEventListener("click", () => {
+    
+    console.log(carrito)
+    
+    let swal_html = ``
+
+    // si el carrito esta vacio
+    if(carrito.length == 0){
+        swal_html = swal_html + `
+            El carrito esta vacio.
+        `
+
+        Swal.fire({
+            title: "Carrito",
+            html: swal_html,        
+            confirmButtonText: "OK",
+        })
+    }
+    // si el carrito tiene data
+    else {
+        swal_html = swal_html + `
+            <table  class="table table-responsive table-striped">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Nombre</th>
+                        <th>Cantidad</th>
+                        <th>Precio</th>
+                    </tr>
+                </thead>
+                <tbody>
+        `
+
+        carrito.forEach((producto) => {
+
+            console.log(producto.nombre)
+
+        })
+
+
+        swal_html = swal_html + `
+            <tr>
+                <td>1</td>
+                <td>Dakota Rice</td>
+                <td>Niger</td>
+                <td>$36,738</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Minerva Hooper</td>
+                <td>Curaçao</td>
+                <td>$36,738</td>
+            </tr>
+        `
+
+        swal_html = swal_html + `
+            </tbody>
+            </table>
+        `
+
+
+        Swal.fire({
+            title: "Carrito",
+            html: swal_html,        
+            confirmButtonText: "Hacer Perdido",
+        })
+        .then((result) => {
+            Swal.fire({
+                title: "Genial!",
+                text: "Le vamo a estar mandando un mail, para enviarle los productos.", 
+                icon: "success",
+                confirmButtonText: "Aceptar",
+            })
+        })
+    }
+
+    
+})
+
+botonBuscar.addEventListener("click", () => buscarProducto(inputBuscador.value))
